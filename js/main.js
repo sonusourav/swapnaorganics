ddfb18729f6c2865162a19a121ede834a249dacb
sonusourav <sonusourav@sharechat.co>
2024-06-03 15:04:23 +0530

    updated team members

--- js/main.js
+++ js/main.js
@@ -77,3 +77,5 @@
     
 })(jQuery);
 
+var companyName = "Swapna Organics"
+
---
+++ js/variables.js
@@ -0,0 +1,6 @@
+
+const companyName = "Swapna Organics";
+
+function updateCompanyName() {
+    document.getElementById('company').innerHTML = companyName;
+}
